88b3c8ea45f70e5969a2adcae8bb09daa088487a
Giovanni Vicentin <giovannifvicentin@gmail.com>
2024-03-13 11:43:07 -0300

    feat: finish the project following the notion

---
+++ main.js
@@ -0,0 +1,60 @@
+alert('Hello World!');
+
+const numberOne = 10;
+const numberTwo = 15;
+
+const sum = numberOne + numberTwo;
+
+alert(sum);
+
+const isNumber = 10;
+
+if (typeof isNumber == 'number') {
+  alert('It is a number');
+} else {
+  alert('It is not a number');
+}
+
+const isString = 'This is a string';
+
+if (typeof isString == 'string') {
+  alert('It is a string');
+} else {
+  alert('It is not a string');
+}
+
+const isBoolean = true;
+
+if (typeof isBoolean == 'boolean') {
+  alert('It is a boolean');
+} else {
+  alert('It is not a boolean');
+}
+
+const sub = numberOne - numberTwo;
+
+alert(sub);
+
+const mult = numberOne * numberTwo;
+
+alert(mult);
+
+const div = numberOne / numberTwo;
+
+alert(div);
+
+const isEvenNumber = 4;
+
+if (isEvenNumber % 2 == 0) {
+  alert('It is an even number');
+} else {
+  alert('It is not an even number');
+}
+
+const isOddNumber = 5;
+
+if (isOddNumber % 2 != 0) {
+  alert('It is an odd number');
+} else {
+  alert('It is not an odd number');
+}
